412c099eb67d69ca5ec9d30ec3f7ceaedc86aa1b
Tarequr Rahman Sabbir <tarequrrahman@nextgenitltd.com>
2025-12-28 16:50:19 +0600

    feat: add date format

--- resources/views/admin/pages/products/stock_out.blade.php
+++ resources/views/admin/pages/products/stock_out.blade.php
@@ -1,53 +1,53 @@
 @extends('admin.layouts.master')
 
 @section('content')
-<div class="container-fluid p-0">
-    <div class="mb-3">
-        <h1 class="h3 d-inline align-middle">Stock Out Products</h1>
-        <a href="{{ route('products.index') }}" class="btn btn-secondary float-end">Back to Product List</a>
-    </div>
+    <div class="container-fluid p-0">
+        <div class="mb-3">
+            <h1 class="h3 d-inline align-middle">Stock Out Products</h1>
+            <a href="{{ route('products.index') }}" class="btn btn-secondary float-end">Back to Product List</a>
+        </div>
 
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="card-title mb-0">Stock Out Product List</h5>
-                </div>
-                <div class="card-body">
-                    <table id="datatables-reponsive" class="table table-striped" style="width:100%">
-                        <thead>
-                            <tr>
-                                <th>SL</th>
-                                <th>Name</th>
-                                <th>Category</th>
-                                <th>Branch</th>
-                                <th>Serial</th>
-                                <th>Status</th>
-                                <th>Date Sold</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($products as $product)
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="card-title mb-0">Stock Out Product List</h5>
+                    </div>
+                    <div class="card-body">
+                        <table id="datatables-reponsive" class="table table-striped" style="width:100%">
+                            <thead>
                                 <tr>
-                                    <td>{{ $loop->iteration }}</td>
-                                    <td>{{ $product->name }}</td>
-                                    <td>{{ $product->category->name ?? 'N/A' }}</td>
-                                    <td>{{ $product->branch->name ?? 'N/A' }}</td>
-                                    <td>{{ $product->serial_no }}</td>
-                                    <td>
-                                        <span class="badge bg-danger">Stock Out</span>
-                                    </td>
-                                    <td>{{ $product->updated_at->format('Y-m-d H:i') }}</td>
+                                    <th>SL</th>
+                                    <th>Name</th>
+                                    <th>Category</th>
+                                    <th>Branch</th>
+                                    <th>Serial</th>
+                                    <th>Status</th>
+                                    <th>Date Sold</th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                @foreach ($products as $product)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $product->name }}</td>
+                                        <td>{{ $product->category->name ?? 'N/A' }}</td>
+                                        <td>{{ $product->branch->name ?? 'N/A' }}</td>
+                                        <td>{{ $product->serial_no }}</td>
+                                        <td>
+                                            <span class="badge bg-danger">Stock Out</span>
+                                        </td>
+                                        <td>{{ $product->updated_at->format('d/m/Y h:i A') }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
 
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
 
 @push('js')
