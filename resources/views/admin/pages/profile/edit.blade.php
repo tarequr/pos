b4e24dbcc2e5b0934a06663083907e756e3c8eb2
Tarequr Rahman Sabbir <tarequrrahman@nextgenitltd.com>
2025-12-28 15:02:15 +0600

    feat: profile details dynamic

--- resources/views/admin/pages/profile/edit.blade.php
+++ resources/views/admin/pages/profile/edit.blade.php
@@ -13,73 +13,39 @@
                         <h5 class="card-title mb-0">Profile Details</h5>
                     </div>
                     <div class="card-body text-center">
-                        <img src="{{ asset('admin/img/avatars/avatar.jpg') }}" alt="Christina Mason"
-                            class="img-fluid rounded-circle mb-2" width="128" height="128">
-                        <h5 class="card-title mb-0">Christina Mason</h5>
-                        <div class="text-muted mb-2">Lead Developer</div>
-
-                        <div>
-                            <a class="btn btn-primary btn-sm" href="#">Follow</a>
-                            <a class="btn btn-primary btn-sm" href="#"><svg xmlns="http://www.w3.org/2000/svg"
-                                    width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                    class="feather feather-message-square">
-                                    <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-                                </svg> Message</a>
+                        <img src="{{ !empty($user->avatar) ? asset('storage/' . $user->avatar) : asset('admin/img/avatars/avatar.jpg') }}"
+                            alt="{{ $user->name }}" class="img-fluid rounded-circle mb-2" width="128"
+                            height="128">
+                        <h5 class="card-title mb-0">{{ $user->name }}</h5>
+                        <div class="text-muted mb-2">{{ $user->designation ?? 'N/A' }}</div>
+                    </div>
+                    @if ($user->address || $user->phone)
+                        <hr class="my-0">
+                        <div class="card-body">
+                            <h5 class="h6 card-title">About</h5>
+                            <ul class="list-unstyled mb-0">
+                                @if ($user->address)
+                                    <li class="mb-1"><svg xmlns="http://www.w3.org/2000/svg" width="24"
+                                            height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                            class="feather feather-home feather-sm me-1">
+                                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                                            <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                                        </svg> Lives in {{ $user->address }}</li>
+                                @endif
+                                @if ($user->phone)
+                                    <li class="mb-1"><svg xmlns="http://www.w3.org/2000/svg" width="24"
+                                            height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                            class="feather feather-phone feather-sm me-1">
+                                            <path
+                                                d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-2.29a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z">
+                                            </path>
+                                        </svg> {{ $user->phone }}</li>
+                                @endif
+                            </ul>
                         </div>
-                    </div>
-                    <hr class="my-0">
-                    <div class="card-body">
-                        <h5 class="h6 card-title">Skills</h5>
-                        <a href="#" class="badge bg-primary me-1 my-1">HTML</a>
-                        <a href="#" class="badge bg-primary me-1 my-1">JavaScript</a>
-                        <a href="#" class="badge bg-primary me-1 my-1">Sass</a>
-                        <a href="#" class="badge bg-primary me-1 my-1">Angular</a>
-                        <a href="#" class="badge bg-primary me-1 my-1">Vue</a>
-                        <a href="#" class="badge bg-primary me-1 my-1">React</a>
-                        <a href="#" class="badge bg-primary me-1 my-1">Redux</a>
-                        <a href="#" class="badge bg-primary me-1 my-1">UI</a>
-                        <a href="#" class="badge bg-primary me-1 my-1">UX</a>
-                    </div>
-                    <hr class="my-0">
-                    <div class="card-body">
-                        <h5 class="h6 card-title">About</h5>
-                        <ul class="list-unstyled mb-0">
-                            <li class="mb-1"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                    stroke-linecap="round" stroke-linejoin="round"
-                                    class="feather feather-home feather-sm me-1">
-                                    <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                                    <polyline points="9 22 9 12 15 12 15 22"></polyline>
-                                </svg> Lives in <a href="#">San Francisco, SA</a></li>
-
-                            <li class="mb-1"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                    stroke-linecap="round" stroke-linejoin="round"
-                                    class="feather feather-briefcase feather-sm me-1">
-                                    <rect x="2" y="7" width="20" height="14" rx="2" ry="2"></rect>
-                                    <path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path>
-                                </svg> Works at <a href="#">GitHub</a></li>
-                            <li class="mb-1"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                    stroke-linecap="round" stroke-linejoin="round"
-                                    class="feather feather-map-pin feather-sm me-1">
-                                    <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-                                    <circle cx="12" cy="10" r="3"></circle>
-                                </svg> From <a href="#">Boston</a></li>
-                        </ul>
-                    </div>
-                    <hr class="my-0">
-                    <div class="card-body">
-                        <h5 class="h6 card-title">Elsewhere</h5>
-                        <ul class="list-unstyled mb-0">
-                            <li class="mb-1"><a href="#">staciehall.co</a></li>
-                            <li class="mb-1"><a href="#">Twitter</a></li>
-                            <li class="mb-1"><a href="#">Facebook</a></li>
-                            <li class="mb-1"><a href="#">Instagram</a></li>
-                            <li class="mb-1"><a href="#">LinkedIn</a></li>
-                        </ul>
-                    </div>
+                    @endif
                 </div>
             </div>
 
